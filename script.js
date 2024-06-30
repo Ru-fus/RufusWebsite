fedfb103abbedc1d55589eac32dc8930d07ff8ba
Rufus <96014957+Ru-fus@users.noreply.github.com>
2024-06-30 15:59:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -9,5 +9,27 @@ const observer = new IntersectionObserver((entries) => {
 	});
 });
 
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((e) => observer.observe(e));
+
+const countdown = () => {
+    const countDate = new Date("Dec 2, 2024 00:00:00").getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % day % hour) / minute);
+    const textSecond = Math.floor((gap % day % hour % minute) / second);
+
+    document.querySelector("#days").innerText = textDay;
+    document.querySelector("#hours").innerText = textHour;
+    document.querySelector("#minutes").innerText = textMinute;
+    document.querySelector("#seconds").innerText = textSecond;
+};
+
+setInterval(countdown, 1000);
+
